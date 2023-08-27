42462980392099a07830b5a34fb97822cf0229b5
DSulzhits <dsulzhits@gmail.com>
2023-08-27 19:26:11 +0300

    Добавлен CRUD для модели Users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
+from users.models import User
 
-# Create your views here.
+
+class UserListAPIView(ListAPIView):
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class UserCreateAPIView(CreateAPIView):
+    queryset = User.objects.all()
+
+
+class UserRetrieveAPIView(RetrieveAPIView):
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class UserUpdateAPIView(UpdateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return User.objects.filter(pk=user.id)
+
+
+# class UserDestroyAPIView(DestroyAPIView):
+#     queryset = User.objects.all()
+#     permission_classes = [IsAuthenticated]
