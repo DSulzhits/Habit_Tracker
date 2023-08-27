c0f30fe439e2a3d0252adabdc5649ccc463b39bf
DSulzhits <dsulzhits@gmail.com>
2023-08-27 16:26:02 +0300

    Добавлена модель User

--- users/models.py
+++ users/models.py
@@ -1,5 +1,29 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.utils.translation import gettext_lazy as _
+from datetime import datetime
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class UserRoles(models.TextChoices):
+    MEMBER = 'member', _('member')
+    MODERATOR = 'moderator', _('moderator')
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Email')
+    role = models.CharField(max_length=9, choices=UserRoles.choices, default=UserRoles.MEMBER)
+    last_login = models.DateTimeField(default=datetime.now, verbose_name="последний вход")
+    is_active = models.BooleanField(default=True, verbose_name="активный")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.email}'
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
