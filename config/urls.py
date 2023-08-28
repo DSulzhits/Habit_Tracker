07623b1bcbf5aa44229e04931f73b175639b3002
DSulzhits <dsulzhits@gmail.com>
2023-08-28 12:56:02 +0300

    Habit Urlpatterns добавлены к базовым Urls

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('habits/', include('habit.urls', namespace='habits')),
     path('users/', include('users.urls', namespace='users')),
 ]
