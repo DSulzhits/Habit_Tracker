5fae2555b5fe4fd7cc1125651632e7b806c18e78
DSulzhits <dsulzhits@gmail.com>
2023-08-28 14:30:45 +0300

    Добавлено задание send_tgbot_message

---
+++ habit/tasks.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from telebot import TeleBot
+from config.celery import app
+from habit.models import Habit
+
+
+@app.task
+def send_tgbot_message(habit_id):
+    habit = Habit.objects.get(id=habit_id)
+    tg_bot = TeleBot(settings.TG_BOT_TOKEN)
+    message = f"Напоминаю время:{habit.time} для {habit.name}, надо сделать {habit.action}! Место {habit.place}."
+    tg_bot.send_message(habit.created_by.telegram_id, message)
