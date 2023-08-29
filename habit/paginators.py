39c2ea16e8bd5f99c5b06bb0a54e048aff3dc9ed
DSulzhits <dsulzhits@gmail.com>
2023-08-29 12:14:00 +0300

    Изменен параметр пагинации

--- habit/paginators.py
+++ habit/paginators.py
@@ -3,6 +3,6 @@ from rest_framework.pagination import PageNumberPagination
 
 class HabitPaginator(PageNumberPagination):
     """Пагинация с заданными параметрами от 3-х до 20 элементов на странице"""
-    page_size = 3
+    page_size = 5
     page_query_param = 'page_size'
     max_page_size = 20
