28dd6f4a620693139b136407b15d9e4e773dfd30
DSulzhits <dsulzhits@gmail.com>
2023-08-29 12:32:55 +0300

    Скорректирован конфиг

--- config/celery.py
+++ config/celery.py
@@ -2,7 +2,7 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'habit.settings')
-app = Celery('habit')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+app = Celery('config')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
